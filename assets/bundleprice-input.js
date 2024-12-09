7250cc96c05a7aa0abe715d67355c2c8cff0c722
muhammad-luqman-brainx <muhammad.luqman@brainxtech.com>
2024-12-09 17:37:19 +0500

    bundle template

--- assets/bundleprice-input.js
+++ assets/bundleprice-input.js
@@ -2,32 +2,36 @@
 
 // Get backorder json from main-product.liquid
 const bundleVariants = JSON.parse(
-  document.querySelector("#ProductJSON-bundleprice").textContent
-);
-
-// Find the hidden input in the buy buttons:
-const bundleInputs = document.querySelectorAll("input#bundleprice");
-document.addEventListener("DOMContentLoaded", function () {
-  // Find the hidden input in the buy buttons
-  const productVariantInput = document.querySelector(".product-variant-id");
-  if (!productVariantInput) {
-    console.error("Product variant input not found.");
-    return;
-  }
-
-  // Event listener for changes to the input
-  productVariantInput.addEventListener("change", function () {
-    // Get appropriate variant from backorderVariants
-    const bundlePrice = bundleVariants[productVariantInput.value];
-    for (let i = 0; i < bundleInputs.length; i++)
-      // If it is backordered set the name and value attributes. If not, remove them.
-      if (bundlePrice) {
-        // Set attributes
-        bundleInputs[i].setAttribute("name", "properties[_BundlePrice]");
-        bundleInputs[i].setAttribute("value", JSON.stringify(bundlePrice?.bundlePrice));
-      } else {
-        bundleInputs[i].setAttribute("name", "");
-        bundleInputs[i].setAttribute("value", "");
-      }
+    document.querySelector("#ProductJSON-bundleprice").textContent
+  );
+  
+  // Find the hidden input in the buy buttons:
+  const bundleInputs = document.querySelectorAll("input#bundleprice");
+  document.addEventListener("DOMContentLoaded", function () {
+    // Find the hidden input in the buy buttons
+    const productVariantInput = document.querySelector(".product-variant-id");
+    if (!productVariantInput) {
+      console.error("Product variant input not found.");
+      return;
+    }
+  
+    // Event listener for changes to the input
+    productVariantInput.addEventListener("change", function () {
+      // Get appropriate variant from backorderVariants
+      const bundlePrice = bundleVariants[productVariantInput.value];
+      for (let i = 0; i < bundleInputs.length; i++)
+        // If it is backordered set the name and value attributes. If not, remove them.
+        if (bundlePrice) {
+          // Set attributes
+          bundleInputs[i].setAttribute("name", "properties[_BundlePrice]");
+          bundleInputs[i].setAttribute(
+            "value",
+            JSON.stringify(bundlePrice?.bundlePrice)
+          );
+        } else {
+          bundleInputs[i].setAttribute("name", "");
+          bundleInputs[i].setAttribute("value", "");
+        }
+    });
   });
-});
+  
